103c2c76dae50bb7d62063db307bfb79ac929bdc
vishnukant.mule <vishnukant.mule@finoux.com>
2025-01-31 16:59:35 +0530

    Resolved Dep Issue

--- index.js
+++ index.js
@@ -3,28 +3,36 @@ const connectDB = require('./db/database');
 const express = require('express');
 const http = require('http');
 
-
-const register=require('./routes/auth/register');
-const login=require('./routes/auth/login');
-
+const register = require('./routes/auth/register');
+const login = require('./routes/auth/login');
 
 const app = express();
-const server = http.createServer(app);
+const server = http.createServer(app); 
+
 app.use(express.json());
 
+connectDB(); 
 
-connectDB();
+app.use('/auth', login);
+app.use('/auth', register);
 
+const PORT = process.env.PORT || 9000;
 
+server.listen(PORT, '0.0.0.0', () => {
+    console.log(`Server is running on port ${PORT}`);
+});
 
+const wss = new WebSocket.Server({ server });
 
-app.use('/auth',login);
-app.use('/auth',register);
+wss.on('connection', (ws, req) => {
+    console.log('New WebSocket connection established.');
 
+    ws.on('message', (message) => {
+        console.log(`Received message: ${message}`);
+        ws.send(`Server received: ${message}`);
+    });
 
-const port = process.env.PORT || 9000;
-
-server.listen(port, () => {
-    console.log(`Server is running on port ${port}!`);
-  });
-
+    ws.on('close', () => {
+        console.log('Client disconnected');
+    });
+});
